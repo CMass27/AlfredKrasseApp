e9cb3c9c29281a778379777b37a4604b4f4c01aa
Marco Schmitz <inf3067@hs-worms.de>
2021-12-20 15:39:40 +0100

    Button hinzufügen, Code zusammenführen

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
-window.iconbitmap('./resources/images/HomeIcon.ico')
+root = Tk()
+root.title('Alfred Krasse App')
+root.iconbitmap("./resources/images/HomeIcon.ico")
+root.geometry("400x700")
+root['background'] = '#FFFEF6'
 
-window.mainloop()
 
+def my_click():
+    myLabel.destroy()
+    myButton.destroy()
+    myLabel2.grid(row=0, column=0)
+
+
+myLabel = Label(root, text="Hello World")
+myLabel2 = Label(root, text="For Dennis <3")
+myButton = Button(root, text="click me", command=my_click)
+
+myLabel.grid(row=0, column=0)
+# myLabel2.grid(row = 1, column = 0)
+myButton.grid(row=3, column=0)
+
+root.mainloop()
